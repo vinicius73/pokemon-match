d7c2dbc9c1434a7586a90461dd52d3de567641af
Vinicius Reis <luiz.vinicius73@gmail.com>
2020-05-31 16:48:58 -0300

    ⚡️ improve build

--- vue.config.js
+++ vue.config.js
@@ -5,24 +5,45 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  configureWebpack: config => {
-    // get a reference to the existing ForkTsCheckerWebpackPlugin
-    const existingForkTsChecker = config.plugins.filter(
-      p => p instanceof ForkTsCheckerWebpackPlugin
-    )[0]
-
-    // remove the existing ForkTsCheckerWebpackPlugin
-    // so that we can replace it with our modified version
-    config.plugins = config.plugins.filter(
-      p => !(p instanceof ForkTsCheckerWebpackPlugin)
-    )
-
-    // copy the options from the original ForkTsCheckerWebpackPlugin
-    // instance and add the memoryLimit property
-    const forkTsCheckerOptions = existingForkTsChecker.options
-    // forkTsCheckerOptions.memoryLimit = 8192
-    forkTsCheckerOptions.workers = 2
-
-    config.plugins.push(new ForkTsCheckerWebpackPlugin(forkTsCheckerOptions))
+  configureWebpack: {
+    node: { Buffer: false },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vuetify: {
+            priority: 7,
+            test: /\/vuetify/,
+            name: 'vuetify',
+            chunks: 'all'
+          },
+          'lodash-es': {
+            priority: 6,
+            test: /\/lodash-es/,
+            name: 'lodash-es',
+            chunks: 'all'
+          }
+        }
+      }
+    }
   }
+  // configureWebpack: config => {
+  //   // get a reference to the existing ForkTsCheckerWebpackPlugin
+  //   const existingForkTsChecker = config.plugins.filter(
+  //     p => p instanceof ForkTsCheckerWebpackPlugin
+  //   )[0]
+
+  //   // remove the existing ForkTsCheckerWebpackPlugin
+  //   // so that we can replace it with our modified version
+  //   config.plugins = config.plugins.filter(
+  //     p => !(p instanceof ForkTsCheckerWebpackPlugin)
+  //   )
+
+  //   // copy the options from the original ForkTsCheckerWebpackPlugin
+  //   // instance and add the memoryLimit property
+  //   const forkTsCheckerOptions = existingForkTsChecker.options
+  //   // forkTsCheckerOptions.memoryLimit = 8192
+  //   forkTsCheckerOptions.workers = 2
+
+  //   config.plugins.push(new ForkTsCheckerWebpackPlugin(forkTsCheckerOptions))
+  // }
 }
