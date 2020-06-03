62940a45120e53e7a2407793dcbb7b0823b481f9
Vinicius Reis <luiz.vinicius73@gmail.com>
2020-06-03 10:18:35 -0300

    ⚡️ improve bundle size
    support only modern browsers

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const pkg = require('./package.json')
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 process.env.VUE_APP_VERSION = pkg.version
 
@@ -24,29 +23,15 @@ module.exports = {
             test: /\/lodash-es/,
             name: 'lodash-es',
             chunks: 'all'
+          },
+          vue: {
+            priority: 7,
+            test: /(\/vue\/|\/vuex\/|\/vue-router\/)/,
+            name: 'vue',
+            chunks: 'all'
           }
         }
       }
     }
   }
-  // configureWebpack: config => {
-  //   // get a reference to the existing ForkTsCheckerWebpackPlugin
-  //   const existingForkTsChecker = config.plugins.filter(
-  //     p => p instanceof ForkTsCheckerWebpackPlugin
-  //   )[0]
-
-  //   // remove the existing ForkTsCheckerWebpackPlugin
-  //   // so that we can replace it with our modified version
-  //   config.plugins = config.plugins.filter(
-  //     p => !(p instanceof ForkTsCheckerWebpackPlugin)
-  //   )
-
-  //   // copy the options from the original ForkTsCheckerWebpackPlugin
-  //   // instance and add the memoryLimit property
-  //   const forkTsCheckerOptions = existingForkTsChecker.options
-  //   // forkTsCheckerOptions.memoryLimit = 8192
-  //   forkTsCheckerOptions.workers = 2
-
-  //   config.plugins.push(new ForkTsCheckerWebpackPlugin(forkTsCheckerOptions))
-  // }
 }
