754858dd09f52031a9e7003bc34b96dc7f66a602
Vinicius Reis <luiz.vinicius73@gmail.com>
2020-06-03 21:04:07 -0300

    ⚡️ use critical.css in index.html

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import '@/assets/critical.css'
 
 Vue.config.productionTip = false
 
