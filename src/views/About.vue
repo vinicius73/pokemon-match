6e3c242bd38045cf902614256180ccc0394c62b3
Vinicius Reis <luiz.vinicius73@gmail.com>
2020-06-05 12:12:56 -0300

    ✨ more about info

--- src/views/About.vue
+++ src/views/About.vue
@@ -12,8 +12,52 @@ export default Vue.extend({
       <v-card dark>
         <div class="d-flex flex-no-wrap justify-space-between">
           <div>
+            <v-card-title>
+              <div>
+                <h3 class="title">Vinicius Reis</h3>
+                <h4 class="subtitle-1">Severino Full-Stack Developer 🤓</h4>
+              </div>
+            </v-card-title>
+            <v-card-text>
+              <p>
+                Married and father a beautiful daughter. <br>
+                I like movies, books, manga, comics, games... <br>
+                That is, I like more things than I have time to do them.
+              </p>
+            </v-card-text>
+          </div>
+          <v-avatar
+            v-if="this.$vuetify.breakpoint.smAndUp"
+            class="ma-3"
+            size="125">
+            <v-img src="https://gravatar.com/avatar/f803c45d62a468e0cb990398c004bd3e?s=400"></v-img>
+          </v-avatar>
+        </div>
+        <v-card-actions>
+          <v-btn href="https://vinicius.dev" target="_blank" text>
+            <v-icon>mdi-web</v-icon>
+            vinicius.dev
+          </v-btn>
+          <v-btn href="https://twitter.com/luizvinicius73" target="_blank" text>
+            <v-icon>mdi-twitter</v-icon>
+            LuizVinicius73
+          </v-btn>
+          <v-btn href="https://github.com/vinicius73" target="_blank" text>
+            <v-icon>mdi-github</v-icon>
+            vinicius73
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col cols="12" sm="6">
+      <v-card color="accent">
+        <div class="d-flex flex-no-wrap justify-space-between">
+          <div>
             <v-card-title class="headline">
-              Pokémon Match - Game Collection
+              <div>
+                <h3 class="title">Pokémon Match</h3>
+                <h4 class="subtitle-1">Game Collection</h4>
+              </div>
             </v-card-title>
             <v-card-text>
               <p>
@@ -23,6 +67,7 @@ export default Vue.extend({
             </v-card-text>
           </div>
           <v-avatar
+            v-if="this.$vuetify.breakpoint.smAndUp"
             class="ma-3 animation-shake"
             size="125"
             tile>
@@ -30,10 +75,10 @@ export default Vue.extend({
           </v-avatar>
         </div>
         <v-card-actions>
-            <v-btn href="https://github.com/vinicius73/pokemon-match" target="__blank" text>
-              <v-icon>mdi-github</v-icon>
-              vinicius73/pokemon-match
-            </v-btn>
+          <v-btn href="https://github.com/vinicius73/pokemon-match" target="_blank" text>
+            <v-icon>mdi-github</v-icon>
+            vinicius73/pokemon-match
+          </v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
