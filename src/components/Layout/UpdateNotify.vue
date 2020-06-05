e4e69344e562265d60c6d20e3408e1728eed063f
Vinicius Reis <luiz.vinicius73@gmail.com>
2020-06-05 00:20:52 -0300

    🚸 improve update notify

--- src/components/Layout/UpdateNotify.vue
+++ src/components/Layout/UpdateNotify.vue
@@ -1,28 +1,54 @@
 <script lang="ts">
 import Vue from 'vue'
-import { mapState } from 'vuex'
+import { mapState, mapMutations } from 'vuex'
 
 export default Vue.extend({
   name: 'UpdateNotify',
+  data: () => ({
+    loading: false
+  }),
   computed: {
     ...mapState(['hasUpdate'])
   },
   methods: {
+    ...mapMutations(['setHasUpdate']),
     update () {
-      window.location.reload()
+      this.loading = true
+      this.$nextTick(() => {
+        window.location.reload()
+      })
+    },
+    close () {
+      this.setHasUpdate(false)
     }
   }
 })
 </script>
 
 <template>
-  <v-snackbar :timeout="0" :value="hasUpdate" top>
-    Has an update available
-    <v-btn
-      @click="update"
-      color="yellow"
-      text>
-      Update
-    </v-btn>
-  </v-snackbar>
+  <v-bottom-sheet dark v-model="hasUpdate" persistent>
+      <v-sheet class="text-center" height="150px">
+        <div class="py-3">
+          There is an update available, update your browser and enjoy the game.
+        </div>
+
+        <v-btn
+          text
+          class="ma-2"
+          color="error"
+          @click="close">
+          <v-icon>mdi-close</v-icon>
+          close
+        </v-btn>
+        <v-btn
+          text
+          :loading="loading"
+          class="ma-2"
+          color="yellow"
+          @click="update">
+          <v-icon>mdi-reload</v-icon>
+          update
+        </v-btn>
+      </v-sheet>
+    </v-bottom-sheet>
 </template>
