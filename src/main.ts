1e02f0c876108214a5ea7729080a8ea74b2b4b53
Vinicius Reis <luiz.vinicius73@gmail.com>
2020-07-03 00:31:42 -0300

    ⚡️ improve performance loading

--- src/main.ts
+++ src/main.ts
@@ -5,27 +5,30 @@ import './registerServiceWorker'
 import './plugins/orientation'
 import '@/assets/main.scss'
 
-const Shell = () => import(/* webpackChunkName: "shell" */'./Shell.vue')
-
 Vue.config.productionTip = false
 
 const bootstrap = async () => {
   const router = await import(/* webpackChunkName: "root" */'./router')
   const store = await import(/* webpackChunkName: "root" */'./store')
   const vuetify = await import(/* webpackChunkName: "plugins-vuetify" */'./plugins/vuetify')
+  const Shell = await import(/* webpackChunkName: "shell" */'./Shell.vue')
 
   new Vue({
     router: router.default,
     store: store.default,
     vuetify: vuetify.default,
     beforeMount () {
-      document.body.classList.remove('waiting-ready')
+      document
+        .body
+        .classList
+        .remove('waiting-ready')
     },
-    render: h => h(Shell)
+    // @ts-ignore
+    render: h => h(Shell.default)
   }).$mount('#app')
 }
 
 setTimeout(() => {
   bootstrap()
     .catch(console.error)
-}, 500)
+}, 900)
