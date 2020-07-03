bb85c9a54df6bbc42dc9ef56f9ec31d5e9a3ae96
Vinicius Reis <luiz.vinicius73@gmail.com>
2020-07-03 10:18:23 -0300

    🚚 split routes file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,39 +1,8 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import VueRouter from 'vue-router'
+import routes from './routes'
 
 Vue.use(VueRouter)
-
-const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    redirect: '/image-match'
-  },
-  {
-    path: '/image-match',
-    name: 'ImageMatch',
-    component: () => import(/* webpackChunkName: "page-image-match" */ '../views/ImageMatch.vue')
-  },
-  {
-    path: '/name-match',
-    name: 'NameMatch',
-    component: () => import(/* webpackChunkName: "page-name-match" */ '../views/NameMatch.vue')
-  },
-  {
-    path: '/type-match',
-    name: 'TypeMatch',
-    component: () => import(/* webpackChunkName: "page-type-match" */ '../views/TypeMatch.vue')
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "page-about" */ '../views/About.vue')
-  },
-  {
-    path: '/debug',
-    name: 'Debug',
-    component: () => import(/* webpackChunkName: "page-debug" */ '../views/Debug.vue')
-  }
-]
 
 const router = new VueRouter({
   mode: 'hash',
---
+++ src/router/routes.ts
@@ -0,0 +1,35 @@
+import { RouteConfig } from 'vue-router'
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    component: () => import(/* webpackChunkName: "page-home" */ '../views/Home.vue')
+  },
+  {
+    path: '/image-match',
+    name: 'ImageMatch',
+    component: () => import(/* webpackChunkName: "page-image-match" */ '../views/ImageMatch.vue')
+  },
+  {
+    path: '/name-match',
+    name: 'NameMatch',
+    component: () => import(/* webpackChunkName: "page-name-match" */ '../views/NameMatch.vue')
+  },
+  {
+    path: '/type-match',
+    name: 'TypeMatch',
+    component: () => import(/* webpackChunkName: "page-type-match" */ '../views/TypeMatch.vue')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "page-about" */ '../views/About.vue')
+  },
+  {
+    path: '/debug',
+    name: 'Debug',
+    component: () => import(/* webpackChunkName: "page-debug" */ '../views/Debug.vue')
+  }
+]
+
+export default routes
